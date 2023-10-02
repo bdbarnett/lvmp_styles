c7b4f7212b19b89be79c2db42a797b3cb25c8488
bdbarnett <127794626+bdbarnett@users.noreply.github.com>
2023-10-02 16:40:44 -0500

    Initial commit

--- examples/styles_simpletest.py
+++ examples/styles_simpletest.py
@@ -1,1 +1,45 @@
+#!/opt/bin/lv_micropython -i
 
+# SPDX-FileCopyrightText: 2023 Brad Barnett
+#
+# SPDX-License-Identifier: MIT
+
+import display_driver
+import lvgl as lv
+import styles
+
+class MyCont(lv.obj):
+    style_key = styles.styles.DEFAULT
+    def __init__(self, parent):
+        super().__init__(parent)
+
+def set_cont2_style(event):
+    if cont2.style_key == styles.styles.DEFAULT:
+        cont2.style_key = styles.styles.ROUND
+    else:
+        cont2.style_key = styles.styles.DEFAULT
+    styles.add_styles_to_children(cont2)
+
+scr = lv.scr_act()
+
+cont1 = lv.obj(scr)
+cont1.set_size(lv.pct(50), lv.pct(100))
+cont1.align(lv.ALIGN.LEFT_MID, 0, 0)
+btn1 = lv.btn(cont1)
+btn1.set_size(lv.pct(50), lv.pct(50))
+btn1.center()
+btn1.add_event(lambda e: styles.add_styles_to_children(cont1), lv.EVENT.SHORT_CLICKED, None)
+label1 = lv.label(btn1)
+label1.set_text("Press Once")
+label1.center()
+
+cont2 = MyCont(scr)
+cont2.set_size(lv.pct(50), lv.pct(100))
+cont2.align(lv.ALIGN.RIGHT_MID, 0, 0)
+btn2 = lv.btn(cont2)
+btn2.set_size(lv.pct(50), lv.pct(50))
+btn2.center()
+btn2.add_event(set_cont2_style, lv.EVENT.SHORT_CLICKED, None)
+label2 = lv.label(btn2)
+label2.set_text("Press Twice")
+label2.center()
