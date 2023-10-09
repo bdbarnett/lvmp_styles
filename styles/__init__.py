64691930a3e0038715e0fcc131914cb0b14121ee
Brad Barnett <127794626+bdbarnett@users.noreply.github.com>
2023-10-09 08:26:11 -0500

    Add files via upload

--- styles/__init__.py
+++ styles/__init__.py
@@ -2,19 +2,42 @@ import lvgl as lv
 
 from . import styles
 
-def add_styles(obj, style_key=None, style_set=styles.default_style_set):
-    if style_key is not None:
-        key = style_key
-    elif hasattr(obj, "style_key") and obj.style_key is not None:
-        key = obj.style_key
-    else:
-        key = type(obj)
-    for selector, style in style_set[key].items():
-        obj.add_style(style, selector)
-
-def add_styles_to_children(cont, style_key=None, style_set=styles.default_style_set, include_self=True):
+def apply_styles(obj, style_key=None, style_set=styles.default_style_set, include_self=True, recurse=True):
     if include_self:
-        add_styles(cont, style_key=style_key, style_set=style_set)
-    for idx in range(cont.get_child_cnt()):
-        child = cont.get_child(idx)
-        add_styles_to_children(cont=child, style_set=style_set, include_self=True)
+        if style_key is not None:
+            key = style_key
+        elif hasattr(obj, "style_key") and obj.style_key is not None:
+            key = obj.style_key
+        else:
+            key = type(obj)
+        for selector, style in style_set[key].items():
+            obj.add_style(style, selector)
+    if recurse:
+        for idx in range(obj.get_child_cnt()):
+            child = obj.get_child(idx)
+            apply_styles(child, style_set=style_set, include_self=True)
+
+
+class Theme(lv.theme_t):
+
+    def __init__(self, disp=None, style_set=styles.default_style_set, base_theme=None):
+        super().__init__()
+
+        self.style_set = style_set
+        if base_theme is None:
+            # This theme is based on active theme (material)
+            base_theme = lv.theme_get_from_obj(lv.scr_act())
+
+        # This theme will be applied only after base theme is applied
+        self.set_parent(base_theme)
+
+        # Set the "apply" callback of this theme to our custom callback
+        self.set_apply_cb(self.apply)
+
+        if disp is None:
+            disp = lv.disp_get_default()
+        # Activate this theme on display
+        disp.set_theme(self)
+    
+    def apply(self, theme, obj):
+        apply_styles(obj, style_set=self.style_set, recurse=False)
